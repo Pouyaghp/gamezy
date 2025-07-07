2fb3834bb8d6be3c9c46ff9cab10bf9a3ea61f06
Pouyaghp <pouyaghp97@gmail.com>
2025-07-07 15:53:22 +0100

    Initial commit

--- database/firebase-config.js
+++ database/firebase-config.js
@@ -1,13 +1,14 @@
 // Your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyAFM0b2VTetnrNmmYgxNKPDvjl8xHH2L_c",
-  authDomain: "baaziroo.firebaseapp.com",
-  projectId: "baaziroo",
-  storageBucket: "baaziroo.firebasestorage.app",
-  messagingSenderId: "952962734370",
-  appId: "1:952962734370:web:1706f02a47b8a69d24ed7b",
-  measurementId: "G-Y2QBNRHPZG"
+  apiKey: "AIzaSyC6IS2j3oz7y6ptblLXT_0j2x5hRlGo67E",
+  authDomain: "gamezy-ada70.firebaseapp.com",
+  projectId: "gamezy-ada70",
+  storageBucket: "gamezy-ada70.firebasestorage.app",
+  messagingSenderId: "81426880253",
+  appId: "1:81426880253:web:e6a1efc4416dc52e675094",
+  measurementId: "G-LEQXB0HNPM"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
